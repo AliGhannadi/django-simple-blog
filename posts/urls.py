3e59906dfd76c6dc7f2291e73d9a033a78c075a8
Ali Ghannadi <95235686+AliGhannadi@users.noreply.github.com>
2026-01-28 17:45:48 +0330

    Added category part - template improvements - Fixed sidebar category & tags part

--- posts/urls.py
+++ posts/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
-from .views import blog_home
-
+from .views import blog_home, post_by_tag, post_by_category
+app_name = 'posts'
 urlpatterns = [
     path('', blog_home, name='blog_home'),
+    path('tag/<slug:tag_slug>/', post_by_tag, name='tag'),
+    path('category/<slug:category_slug>', post_by_category, name='cat')
+    
 ]
--- posts/views.py
+++ posts/views.py
@@ -1,6 +1,36 @@
 from django.shortcuts import render
 from django.utils import timezone
-from .models import Post
+from django.shortcuts import get_object_or_404
+from .models import Post, Tag, Category
 def blog_home(request):
     posts = Post.objects.filter(status=1, created_date__lte=timezone.now()).order_by('-created_date')
-    return render(request, 'posts/blog.html', {'posts': posts})
+    tags = Tag.objects.all()
+    categories = Category.objects.all()
+    return render(request, 'posts/blog.html', {
+        'posts': posts, 
+        'tags': tags,
+        'categories': categories    
+    })
+def post_by_tag(request, tag_slug):
+    tag = get_object_or_404(Tag, slug=tag_slug)
+    posts = Post.objects.filter(tag=tag, status=1).order_by('-created_date')
+    tags = Tag.objects.all()
+    categories = Category.objects.all()
+    return render(request, 'posts/blog.html', {
+        'posts': posts,
+        'current_tag': tag,
+        'tags': tags,
+        'categories': categories
+        
+    })
+    
+def post_by_category(request, category_slug):
+    category = get_object_or_404(Category, slug=category_slug)
+    posts = Post.objects.filter(category=category, status=1).order_by('-created_date')
+    categories = Category.objects.all()
+    return render(request, 'posts/blog.html', {
+        'posts': posts,
+        'current_category': category,
+        'categories': categories       
+        
+    })
