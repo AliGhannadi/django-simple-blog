f590b31d8aef476ace1dbdb44e132d7c53fd792e
Ali Ghannadi <95235686+AliGhannadi@users.noreply.github.com>
2026-01-30 17:11:53 +0330

    Added search box

--- posts/urls.py
+++ posts/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import blog_home, post_by_tag, post_by_category, single_post
+from .views import blog_home, post_by_tag, post_by_category, single_post, blog_search
 app_name = 'posts'
 urlpatterns = [
     path('', blog_home, name='blog_home'),
     path('tag/<slug:tag_slug>/', post_by_tag, name='tag'),
     path('category/<slug:category_slug>', post_by_category, name='cat'),
     path("<int:pid>/", single_post, name="single"),
+    path('search/', blog_search, name="search")
 ]
--- posts/views.py
+++ posts/views.py
@@ -1,11 +1,14 @@
 from django.shortcuts import render
 from django.utils import timezone
 from django.shortcuts import get_object_or_404
+from django.db.models import Count, Q
 from .models import Post, Tag, Category
 def blog_home(request):
     posts = Post.objects.filter(status=1, created_date__lte=timezone.now()).order_by('-created_date')
     tags = Tag.objects.all()
-    categories = Category.objects.all()
+    categories = Category.objects.annotate(
+        post_count=Count('posts', distinct=True)   
+    )
     return render(request, 'posts/blog.html', {
         'posts': posts, 
         'tags': tags,
@@ -15,7 +18,9 @@ def post_by_tag(request, tag_slug):
     tag = get_object_or_404(Tag, slug=tag_slug)
     posts = Post.objects.filter(tag=tag, status=1).order_by('-created_date')
     tags = Tag.objects.all()
-    categories = Category.objects.all()
+    categories = Category.objects.annotate(
+        post_count=Count('posts', distinct=True)   
+    )
     return render(request, 'posts/blog.html', {
         'posts': posts,
         'current_tag': tag,
@@ -27,7 +32,9 @@ def post_by_tag(request, tag_slug):
 def post_by_category(request, category_slug):
     category = get_object_or_404(Category, slug=category_slug)
     posts = Post.objects.filter(category=category, status=1).order_by('-created_date')
-    categories = Category.objects.all()
+    categories = Category.objects.annotate(
+        post_count=Count('posts', distinct=True)   
+    )
     return render(request, 'posts/blog.html', {
         'posts': posts,
         'current_category': category,
@@ -39,9 +46,31 @@ def single_post(request, pid):
     post = get_object_or_404(Post, id=pid)
     post.counted_views += 1
     post.save()
-    categories = Category.objects.all()
+    categories = Category.objects.annotate(
+        post_count=Count('posts', distinct=True)   
+    )
     return render(request, 'posts/single.html',{
         'post': post,
         'categories': categories,
         
     })
+    
+def blog_search(request):
+    posts = Post.objects.filter(status=1, created_date__lte=timezone.now())
+    if s := request.GET.get('s'):
+        posts = posts.filter(
+            Q(title__icontains=s) |
+            Q(content__icontains=s)
+            
+        )
+    categories = Category.objects.annotate(
+        post_count=Count('posts', distinct=True)
+    )
+    tags = Tag.objects.all()
+    return render(request, 'posts/blog.html', {
+        'posts': posts,
+        'categories': categories,
+        'tags': tags,
+        'search_query': request.GET.get('s', '')
+    })
+        
